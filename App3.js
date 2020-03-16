46c4ac78d1365fe3b7590c38dbd3e809a73eb1b3
askariabidi <smohammadaskariabidi@khi.iba.edu.pk>
2020-03-16 05:00:29 +0500

    Updated

---
+++ App3.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class App3 extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default App3
